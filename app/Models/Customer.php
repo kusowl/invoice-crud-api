286190eb2068bc52c4e157d9e22e20389b05ec34
kusowl <kushal.saha@example.com>
2025-09-28 08:31:05 +0530

    feat : Store Customer Record

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -11,6 +11,16 @@ class Customer extends Model
     /** @use HasFactory<\Database\Factories\CustomerFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'type',
+        'email',
+        'address',
+        'city',
+        'state',
+        'postal_code',
+    ];
+
     public function invoices(): HasMany
     {
         return $this->hasMany(Invoice::class);
